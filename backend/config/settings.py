0da1ebd65be90e22607f9cee1a60aa1b94b53595
SAMIJIHEN <jihenesami04@gmail.com>
2026-05-05 13:43:54 +0200

    Fix: suppression .env, requirements minimal

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -12,10 +12,7 @@ Django settings for config project.
 from pathlib import Path
 import os
 import dj_database_url
-from dotenv import load_dotenv
 
-# Charger les variables d'environnement
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
