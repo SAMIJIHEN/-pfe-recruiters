ed92028b9a959d555baf3c872e078d74e7c28056
SAMIJIHEN <jihenesami04@gmail.com>
2026-05-05 14:29:20 +0200

    Fix: utilisation VITE_API_URL pour Railway

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -6,7 +6,8 @@
 
 import axios from "axios";
 
-const API_BASE_URL = "http://127.0.0.1:8000/api";
+// ✅ Correct
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
